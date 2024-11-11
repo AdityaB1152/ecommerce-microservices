3dd04ac76523329ede3519dbcb53e07d6498081f
Aditya <aditya.bonde1152@gmail.com>
2024-11-11 18:01:39 +0530

    User and Order Microservices done

--- user-service/index.js
+++ user-service/index.js
@@ -8,3 +8,18 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/register', async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send('User registered successfully');
+});
+
+app.listen(3001, () => console.log('User Service running on port 3001'));
